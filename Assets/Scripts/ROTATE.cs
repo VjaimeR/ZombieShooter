88c6965beb12de2dd0829ec04ea3d869759fc762
VjaimeR <jaime235689@gmail.com>
2025-01-22 20:02:42 -0600

    Added boolean and weapon

--- Assets/Scripts/ROTATE.cs
+++ Assets/Scripts/ROTATE.cs
@@ -4,15 +4,26 @@ public class ROTATE : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 5;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private bool _isRotating = true;
+    
+    public bool IsRotating
     {
-        
+        get {return _isRotating;}
+        set {_isRotating = value;}
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+       RotateWeapon();
+    }
+
+    private void RotateWeapon()
+    {
+        if (_isRotating)
+        {
         gameObject.transform.Rotate(0f, _rotateSpeed * Time.deltaTime, 0f);
+        }
     }
 }
